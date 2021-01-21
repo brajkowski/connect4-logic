f3e099c893a1f34d9cfc48b8b6025c4d110c5324
Brandon <brandon.rajkowski@gmail.com>
2021-01-20 20:39:29 -0700

    added all library exports to index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { BitboardLogic } from './logic/bitboard-logic';
+export { BitboardPlayerState } from './logic/bitboard-player-state';
+export { FullColumnError } from './logic/full-column-error';
+export { Logic, Player, WinType } from './logic/logic';
+export { PlayerState } from './logic/player-state';
+export { PositionAlreadyOccupiedError } from './logic/position-already-occupied-error';
+export { Constants } from './util/constants';
